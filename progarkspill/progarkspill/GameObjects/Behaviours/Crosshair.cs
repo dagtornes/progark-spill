15eae302b6fa3990275585b9ed5369abc74d5220
Dag Øyvind Tornes <dagtornes@gmail.com>
2011-04-03 16:43:28 +0200

    - Crosshair stays close to player.

--- progarkspill/progarkspill/GameObjects/Behaviours/Crosshair.cs
+++ progarkspill/progarkspill/GameObjects/Behaviours/Crosshair.cs
@@ -9,13 +9,19 @@ namespace progarkspill.GameObjects.Behaviours
 {
     public class Crosshair : Player
     {
+        private static float maxDelta = 750;
 
         public override void decide(Entity me, GameState environment, float timedelta, GameStateStack states)
         {
             // Copy players speed, then add our own delta
             // HACK: We duplicate the players motion here, then add our own.  Not pretty.
             me.Physics.Position += me.Source.Physics.Velocity * me.Source.Physics.Speed * timedelta;
-            me.Physics.Velocity = new Vector2(1, -1) * GamePad.GetState(control).ThumbSticks.Right;
+            Vector2 delta = me.Physics.Position - me.Source.Physics.Position;
+            
+            if (delta.LengthSquared() < maxDelta * maxDelta)
+                me.Physics.Velocity = new Vector2(1, -1) * GamePad.GetState(control).ThumbSticks.Right;
+            else
+                me.Physics.Velocity = -delta;
         }
     }
 }
